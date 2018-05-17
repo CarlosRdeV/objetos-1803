dc58c7ea7c3cde50d26536e50f3adb9e2edbcc50
T-101 <T-101@PC236.mshome.net>
2018-05-17 18:59:29 -0500

    Test

---
+++ objetos-1803/src/objetos/pkg1803/Objetos1803.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos.pkg1803;
+
+/**
+ *
+ * @author T-101
+ */
+public class Objetos1803 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
