1f9f22b2fbe982c2184ea8c100f9cc88b77c3c7b
T-101 <T-101@PC236.mshome.net>
2018-05-17 19:45:14 -0500

    Test2

---
+++ objetos-1803/src/abstraccion/Grados.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion;
+
+/**
+ *
+ * @author T-101
+ */
+public class Grados {
+
+    private float centrigrados;
+    private float fahrenheit;
+
+    public float getCentrigrados() {
+        return centrigrados;
+    }
+
+    public void setCentrigrados(float centrigrados) {
+        this.centrigrados = centrigrados;
+    }
+
+    public float getFahrenheit() {
+        return fahrenheit;
+    }
+
+    public void setFahrenheit(float fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+}
