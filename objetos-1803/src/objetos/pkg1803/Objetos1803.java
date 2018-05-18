b3b82ff2ab460c8b0ce0c14d642cd39c98e196ce
T-101 <T-101@PC236.mshome.net>
2018-05-17 19:03:10 -0500

    Test

--- objetos-1803/src/objetos/pkg1803/Objetos1803.java
+++ objetos-1803/src/objetos/pkg1803/Objetos1803.java
@@ -16,6 +16,7 @@ public class Objetos1803 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Una actualizacion desde el repositorio");
     }
     
 }
