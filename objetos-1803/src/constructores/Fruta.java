19cb732f134bbcbc412d20c1ecf0adcadac8d1ec
T-101 <T-101@PC212>
2018-05-28 19:45:47 -0500

    Tipos de datos

---
+++ objetos-1803/src/constructores/Fruta.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+/**
+ *
+ * @author T-101
+ */
+public class Fruta {
+    
+    String nombre;
+    float precio;
+    
+    //Este es el constructor por defecto
+    public Fruta(){
+        nombre="Persimo";
+     
+    }
+    
+}
---
+++ objetos-1803/src/constructores/ProbarConstructores.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+/**
+ *
+ * @author T-101
+ */
+public class ProbarConstructores {
+ 
+    public static void main(String[] args) {
+        //Vamos a crear una fruta
+        Fruta f1=new Fruta();
+        //sout
+        System.out.println("El nombre de la fruta es "+f1.nombre);
+        System.out.println("El precio de la fruta es "+f1.precio);
+        //El valor por defecto de un String es "null"
+        //El valor por defecto de un Float es "0.0"
+        float valor;
+        //System.out.println(valor);
+    }
+}
